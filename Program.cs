a89e0f0045301725872a089af848685410a4ad46
Yaritza <yaritzamicaelleb@gmail.com>
2021-03-08 19:30:18 -0300

    Tarefa Cebolinha

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+            string textoModificado;
+
+            Console.Write("Escreva algo para falar como o Cebolinha: ");
+            texto= Console.ReadLine();
+
+            textoModificado= texto.Replace ("r","l");
+
+            Console.WriteLine($"Sua fala: {textoModificado}");
+
+        }
+    }
+}
